70b1688485be7949508ae680980072606b20bb71
Gabriel Ramos Takeda <gabrieel.rt@gmail.com>
2015-05-28 16:03:35 -0300

    Remover parênteses maldito no sed do httpd.conf.

--- bash/server-configuration.sh
+++ bash/server-configuration.sh
@@ -3,7 +3,7 @@
 ###
 # Change DocumentRoot
 ###
-sed -i 's/\/var\/www\/html)/\/var\/www/g' /etc/httpd/conf/httpd.conf
+sed -i 's/\/var\/www\/html/\/var\/www/g' /etc/httpd/conf/httpd.conf
 if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /srv/vm206 /var/www
